cbf9fbca0c9f09738ad8580b19d32806ad8aeb86
Gio Liao <gio.liao@nogle.com>
2017-10-20 12:30:08 +0800

    i18n

---
+++ js/i18n.js
@@ -0,0 +1,40 @@
+var i18n = {
+	zh:{
+		//zh-tw
+		tagline: '歡迎來到我們的世界',
+		tagsub: '我們提升您的商業競爭力',
+		abouttitle: '關於我們',
+		aboutdes: '我們團隊專注彈性化核心模組提供於佈署與管理的關鍵技術',
+		servicetitle: '我們的服務',
+		servicedes: '持續創新與改造是我們使命',
+		service01: '創新',
+		service02: '設計',
+		service03: '轉型',
+		experttitle: '我們的專長',
+		expertdes: '睿世團隊專注科技趨勢與系統架構研究,進一步建置先進系統。',
+		contacttitle: '聯絡我們',
+		contactdes: '想進一步了我們的解決方案嗎？',
+		address: '114 台北市內湖區洲子街57號6樓',
+		email: '聯絡我們',
+		join: '加入我們'
+	},
+	en:{
+		//en-us
+		tagline: 'Welcome into our world',
+		tagsub: 'We enhance your business competitiveness',
+		abouttitle: 'ABOUT US',
+		aboutdes: 'Our team focus on flexible core modules to provide key technologies for deployment and management.',
+		servicetitle: 'Our Services',
+		servicedes: 'Continuous innovation and transformation is our mission',
+		service01: 'Innovation',
+		service02: 'Design',
+		service03: 'Transformation',
+		experttitle: 'Our Expertise',
+		expertdes: 'Nexio team focuses on technology trends and system architecture research to further build advanced systems.',
+		contacttitle: 'Contact Us',
+		contactdes: 'Get in touch with us',
+		address: '6F,No. 57, Zhouzi Street, Neihu District, Taipei City 114 , Taiwan',
+		email: 'Email Us',
+		join: 'Join Us'
+	}
+}
--- js/script.js
+++ js/script.js
@@ -68,6 +68,25 @@ $(window).scroll(function() {
 
 })
 
+// lang
+function setLang(e) {
+    var title = Object.keys(e);
+    for (var i = 0; i < title.length; i++) {
+        $('[text=' + title[i] + ']').html(e[title[i]]);
+    };
+}
+
+$('.lang').click(function(){
+	$(this).toggleClass('zh');
+	if ( $('.lang').hasClass('zh') ) {
+		setLang(i18n.zh);
+	} else {
+		setLang(i18n.en);
+	}
+})
+
+setLang(i18n.en);
+
 // map
 function initMap() {
 	var nexio = {
