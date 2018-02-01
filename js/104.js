feebd7535340dcf0a771fb066787f6615094bcab
Gio.Liao <>
2018-02-01 17:26:21 +0800

    104
    104

--- js/104.js
+++ js/104.js
@@ -138,78 +138,78 @@ function __be104MoreThing(o) {
 };
 j104Widget.prototype.jobdata = {
 	data: [{
-		"APPEAR_DATE": "20180116",
-		"C": "4e3c446d3638406932343c653a40381b82b2b2b29435e382664j01",
-		"DESCRIPTION": "As an entertainment tech venture of Nogle, Nexio delivers value driven online \ngaming products including captivating games and a complete backend \nmanagement system for online gaming businesses.\n\nRoles and Responsibilities :\n- Understand the requirements of a project so as to design the systems infrastructure\n- Design the systems infrastructure to ensure high availability and security\n- Implement / configure systems (CentOS)\n- Troubleshoot systems problems\n- Secure systems by implementing policies / access\n- Monitoring of systems performance to ensure maximum usage of systems resources\n- Assist with the continue integration and continuous delivery of codes into staging / production environment\n- Implement automation tools to enhance the productivity of systems team\n\nRequirement:\n- Layer7 service configuration, such as Nginx, Firewall, etc.\n- Use automation framework to apply in enterprise environment, such as \nAnsible, Puppet.\n- Integrate monitoring and alert system to easily capture system/application \nabnormal behavior\n- Working experience to setup container or provision environment to support \nCI/CD, such as Docker, Open Stack\n- Able to write shell script to automate routine tasks\n\nSkill:\n- CentOS / Linux\n- Shell scripting / python\n- Setup / configuration of Kubernetes, Docker, Nginx, Nagios, Kafka, \nZookeeper, Ansible, Graphite, etc\n- Linux trouble shooting tools and commands\n- Layer4 to Layer7 concept",
-		"INDCAT": "Information Technology",
-		"J": "483a44265e6c492434583b1f402e365f2383a426b34363e673c423a1d1d1d1d1d04570363189j56",
-		"JOB": "System Engineer - Nexio (Nogle Venture)",
+		"APPEAR_DATE": "20180201",
+		"C": "3c4e466f383a426b34363e673c423a1d02d2d2d2d464a5c2644j99",
+		"DESCRIPTION": "Responsibilities: \n- Monitor and order office and pantry supplies \n- Be the liaison of vendors and taking care of the general maintenance of the office \n- Greeting and directing customers in a professional and timely manner (phone and in person) \n- Organize staff events and other team building activities ( birthday, annual dinner party, etc) \n- Basic back office financial management (budget control, and process payment request & data analysis (annual budget preparation) \n- Other general administration duties as required \n\n- 行政庶務工作(辦公用品、茶水間等用品購買) \n- 與廠商協調溝通及辦公用品/資產相關維護作業 \n- 電話及訪客接待 \n- 公司活動規劃與執行 (生日活動，年度尾牙等) \n- 行政管理費用之財務管理(採購預算控制及請款作業)及資料分析(年度預算規劃) \n- 主管其他交辦事項",
+		"INDCAT": "軟體及網路相關業",
+		"J": "4b514b30657350295d49432833453d6643f4149723b3d456e434941242424246644d41493816j98",
+		"JOB": "Office Administrator",
 		"JOB_ADDR_NO_DESCRIPT": "台北市內湖區",
 		"MAJOR_CAT_DESCRIPT": "類目@類目@類目",
 		"MINBINARY_EDU": "8",
 		"MINBINARY_EDU_DESC": "大學（學院）",
-		"NAME": "Nogle Taiwan Limited_諾傑股份有限公司",
-		"PERIOD": "03",
-		"PERIOD_DESC": "二年以上工作經驗"
-	}, {
-		"APPEAR_DATE": "20180116",
-		"C": "4e3c446d3638406932343c653a40381b82b2b2b29435e382664j01",
-		"DESCRIPTION": "A fast growing online gaming supplier is looking for an Accountant to be based in Taiwan. \n\nJob Responsibilities:\n- Conduct all day-to-day accounting and finance-related work (AP/AR/GL/Reporting/Tax/Treasure)\n- Manage Accounting System - NAV (Build up & launch new accounting system-NAV \n- Prepare budget, forecast and needed variance analysis\n- Ad hoc financial improvement projects and initiatives",
-		"INDCAT": "Information Technology",
-		"J": "503a44245e6c492434583b1f402e365f2383a426b34363e673c423a1d1d1d1d1a74460363189j48",
-		"JOB": "Accountant - Nexio (Nogle Venture)",
-		"JOB_ADDR_NO_DESCRIPT": "台北市內湖區",
-		"MAJOR_CAT_DESCRIPT": "類目@類目@類目",
-		"MINBINARY_EDU": "8",
-		"MINBINARY_EDU_DESC": "大學（學院）",
-		"NAME": "Nogle Taiwan Limited_諾傑股份有限公司",
+		"NAME": "睿世軟體科技股份有限公司",
 		"PERIOD": "02",
 		"PERIOD_DESC": "一年以上工作經驗"
 	}, {
-		"APPEAR_DATE": "20180116",
-		"C": "4e3c446d3638406932343c653a40381b82b2b2b29435e382664j01",
-		"DESCRIPTION": "Job Description:\n▪ Requirements analysis. Read and understand business requirements from \ndeveloper to suggest better design solution\n▪ Build database foundation skills, like interpreting execution plan, functions, design principal, trouble shooting, etc\n▪ Identify critical monitoring metrics regarding to database, No SQL, and \nanalysis package\n▪ Capable of using script to automate deployment process during each \nregular/hotfix build.\n▪ Research cutting edge solutions to enrich current tools, management \nconsole, etc\n▪ Demonstrate expertise on database, NoSQL related experience when \ndevelopment suffers from slow performance or high resource consumption\n▪ Work with team members to communicate impact, risk and performance \ngain when utilizing new solution\n▪ Perform database related design review, especially on schema design, sql, \nindex, etc\n\nRequirement\n▪ 1-3-year experience in PostgreSQL/MySQL/Oracle operation\n▪ Experience in bottleneck identification, related to SQL/NoSQL operations\n▪ Familiar with database day to day operation items, such as backup, restore, \nupgrade\n▪ Experience with system and database monitoring, including network \nconnection, system status, sql execution plan\n▪ Experience with utilizing tools, scripts to solve routine tasks\n\nSkill\n▪ ANSI SQL and PostgreSQL extension syntax\n▪ Database Algebra and join performance\n▪ Performance tuning on system kernel parameters\n▪ Implement security checks, audit, reports on database machines\n▪ Provide data modeling and ETL methodology\n▪ NoSQL design and tuning is plus\n▪ In memory and analysis solution is plus",
-		"INDCAT": "Information Technology",
-		"J": "5751492e6573502b3b5f422647353d6643f4149723b3d456e434941242424242314c77493816j55",
-		"JOB": "DBA Developer - Nexio (Nogle Venture)",
+		"APPEAR_DATE": "20180201",
+		"C": "3c4e466f383a426b34363e673c423a1d02d2d2d2d464a5c2644j99",
+		"DESCRIPTION": "- Quick prototyping web content including working with design team and backend team and demonstrate quality work on presentation layer to have user experience design consideration. \n- Understand debugging tools using react and other browser tools to facilitate trouble shooting and performance tuning \n- Know how to better structure react component, flow using framework and design good hierarchy of CSS layout. With SEO or cross browser support experience or RWD design is big plus. \n- Familiar with build procedure and configuration to support different staging environment. \n- Help to pinpoint missing APIs and correct API including URL, status code, request/response with RESTful API concept.",
+		"INDCAT": "軟體及網路相關業",
+		"J": "434943285d6b482155413b202b3d355e13739416a33353d663b41391c1c1c1c6194339393098j51",
+		"JOB": "Frontend Developer",
 		"JOB_ADDR_NO_DESCRIPT": "台北市內湖區",
 		"MAJOR_CAT_DESCRIPT": "類目@類目@類目",
 		"MINBINARY_EDU": "8",
 		"MINBINARY_EDU_DESC": "大學（學院）",
-		"NAME": "Nogle Taiwan Limited_諾傑股份有限公司",
+		"NAME": "睿世軟體科技股份有限公司",
+		"PERIOD": "02",
+		"PERIOD_DESC": "一年以上工作經驗"
+	}, {
+		"APPEAR_DATE": "20180201",
+		"C": "3c4e466f383a426b34363e673c423a1d02d2d2d2d464a5c2644j99",
+		"DESCRIPTION": "- Work with team members on designing, developing, and unit-testing new features and modifications \n- Design data structure and utilize appropriate design pattern to model and Implement business logics \n- Document source code and generate readable API documentations \n- Master software engineering tools like Git, Jenkins, and Maven",
+		"INDCAT": "軟體及網路相關業",
+		"J": "484e482d62704d265a46402530423a6313c3e466f383a426b40463e21212121664484e523543j56",
+		"JOB": "Backend Developer",
+		"JOB_ADDR_NO_DESCRIPT": "台北市內湖區",
+		"MAJOR_CAT_DESCRIPT": "類目@類目@類目",
+		"MINBINARY_EDU": "8",
+		"MINBINARY_EDU_DESC": "大學（學院）",
+		"NAME": "睿世軟體科技股份有限公司",
+		"PERIOD": "02",
+		"PERIOD_DESC": "一年以上工作經驗"
+	}, {
+		"APPEAR_DATE": "20180131",
+		"C": "3c4e466f383a426b34363e673c423a1d02d2d2d2d464a5c2644j99",
+		"DESCRIPTION": "We are looking for a detailed-oriented assistant to join our newly formed business development team. \n\n- Assist business development and operation\n- Update and maintain related documents \n- Coordinate general administrative tasks \n- Ad hoc projects as assigned",
+		"INDCAT": "軟體及網路相關業",
+		"J": "3f3d456e3b6f4c2559453f242f41396203b3d456e3739416a3f453d202020201d0473d512a57j02",
+		"JOB": "Business Development Assistant",
+		"JOB_ADDR_NO_DESCRIPT": "台北市內湖區",
+		"MAJOR_CAT_DESCRIPT": "類目@類目@類目",
+		"MINBINARY_EDU": "8",
+		"MINBINARY_EDU_DESC": "大學（學院）",
+		"NAME": "睿世軟體科技股份有限公司",
 		"PERIOD": "03",
 		"PERIOD_DESC": "二年以上工作經驗"
 	}, {
-		"APPEAR_DATE": "20180111",
-		"C": "4e3c446d3638406932343c653a40381b82b2b2b29435e382664j01",
-		"DESCRIPTION": "- To assist in the analysis of design specifications in order to produce comprehensive test scripts and scenarios for expected results to be performed manually and using automated testing tools\n- To assist in the system testing of new applications and existing software enhancements to ensure they meet design specifications.\n- To assist in Functionality Testing of new applications and existing software enhancements to ensure they meet business requirements through performing end to end business scenario tests \n- To assist in Regression Testing of new applications and existing software enhancements to identify any possible impacts caused by changes. \n- Recording of test results \n- To assist in the maintenance and management of manual and automated scripts & test databases \n- To assist in the verification and evidence of reproduction of any reported faults in existing software. \n- To assist in the testing of fixes required for reported faults, including tests for possible impacts from changes.\n- Ensure all work produced meets customer requirements before being released for internal approval \n- Ensure all work meets the guidelines & standards defined for the team and the company \n- Identify and resolve errors in work\n- To follow up with the fault report cases and cooperate closely with related engineers\n- Implement automated testing and tools to improve the efficiency and effectiveness\n- Involvement in development stage",
-		"INDCAT": "Information Technology",
-		"J": "6365472a63714e29395d402445333b6423d3f4770393b436c41473f222222222194b4f4f3634j51",
-		"JOB": "Software QA Tester - Nexio (Nogle Venture)",
+		"APPEAR_DATE": "20180126",
+		"C": "3c4e466f383a426b34363e673c423a1d02d2d2d2d464a5c2644j99",
+		"DESCRIPTION": "- To assist in the system testing of new applications and existing software enhancements to ensure they meet design specifications. \n- To assist in Functionality Testing of new applications and existing software enhancements to ensure they meet business requirements through performing end to end business scenario tests \n- To assist in Regression Testing of new applications and existing software enhancements to identify any possible impacts caused by changes. \n- Recording of test results \n- To assist in the maintenance and management of manual and automated scripts & test databases \n- To assist in the verification and evidence of reproduction of any reported faults in existing software. \n- To assist in the testing of fixes required for reported faults, including tests for possible impacts from changes.\n- Ensure all work produced meets customer requirements before being released for internal approval \n- Ensure all work meets the guidelines & standards defined for the team and the company \n- Identify and resolve errors in work\n- To follow up with the fault report cases and cooperate closely with related engineers\n- Implement automated testing and tools to improve the efficiency and effectiveness\n- Involvement in development stage",
+		"INDCAT": "軟體及網路相關業",
+		"J": "393b436c396d4a2357433d222d3f37608393b436c35373f683d433b1e1e1e1e620463b472a77j57",
+		"JOB": "Software QA Tester",
 		"JOB_ADDR_NO_DESCRIPT": "台北市內湖區",
 		"MAJOR_CAT_DESCRIPT": "類目@類目@類目",
 		"MINBINARY_EDU": "8",
 		"MINBINARY_EDU_DESC": "大學（學院）",
-		"NAME": "Nogle Taiwan Limited_諾傑股份有限公司",
+		"NAME": "睿世軟體科技股份有限公司",
 		"PERIOD": "00",
 		"PERIOD_DESC": "工作經驗不拘"
-	}, {
-		"APPEAR_DATE": "20180111",
-		"C": "4e3c446d3638406932343c653a40381b82b2b2b29435e382664j01",
-		"DESCRIPTION": "Responsibilities:\n- Monitor and order office and pantry supplies\n- Be the liaison ofvendors and taking care of the general maintenance of the office\n- Greeting and directing customers in a professional and timely manner (phone and in person)\n- Organize staff events and other team building activities ( birthday, annual dinner party, etc)\n- Basic back office financial management (budget control, and process payment request & data analysis (annual budget preparation)\n- Other general administration duties as required\n \n- 行政庶務工作(辦公用品、茶水間等用品購買)\n- 與廠商協調溝通及辦公用品/資產相關維護作業\n- 電話及訪客接待\n- 公司活動規劃與執行 (生日活動，年度尾牙等)\n- 行政管理費用之財務管理(採購預算控制及請款作業)及資料分析(年度預算規劃)\n- 主管其他交辦事項",
-		"INDCAT": "Information Technology",
-		"J": "66684a2d6674512c3c60432748363e67540424a733c3e466f444a42252525252424e42463907j54",
-		"JOB": "Office Administrator 行政助理 - Nexio (Nogle Venture)",
-		"JOB_ADDR_NO_DESCRIPT": "台北市內湖區",
-		"MAJOR_CAT_DESCRIPT": "類目@類目@類目",
-		"MINBINARY_EDU": "8",
-		"MINBINARY_EDU_DESC": "大學（學院）",
-		"NAME": "Nogle Taiwan Limited_諾傑股份有限公司",
-		"PERIOD": "02",
-		"PERIOD_DESC": "一年以上工作經驗"
 	}]
 };
-j104Widget.prototype.query_desc = "nexio、自訂公司130000000057618";
+j104Widget.prototype.query_desc = "自訂公司130000000109986";
 j104Widget.prototype.spec_desc = "";
 j104Widget.prototype.__random_ = function() {
 	return Math.round(Math.random() * 9999999999999);
@@ -295,7 +295,7 @@ j104Widget.prototype.output = function() {
 			i++;
 		}
 		if (i >= 20) {
-			str += "<span class='qjob104_shfull' style='font-family:\"細明體\",arial, serif !important;float:right !important;margin-bottom:10px !important; margin-right:15px !important;'><a href='http://www.104.com.tw/104i/joblist.cfm?i_from=qjob&kws=nexio&c=130000000057618'>看完整結果</a></span></div>";
+			str += "<span class='qjob104_shfull' style='font-family:\"細明體\",arial, serif !important;float:right !important;margin-bottom:10px !important; margin-right:15px !important;'><a href='http://www.104.com.tw/104i/joblist.cfm?i_from=qjob&c=130000000109986'>看完整結果</a></span></div>";
 		} else {
 			str += "</div>";
 		}
